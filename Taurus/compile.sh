797baaca0ab120df1e0fc5e53a324fe7553c9099
paschun <152430265+paschun@users.noreply.github.com>
2024-05-15 14:51:31 -0700

    Add wasm to compile.sh

--- Taurus/compile.sh
+++ Taurus/compile.sh
@@ -1,6 +1,22 @@
 #!/usr/local/bin/bash
+
+#### config
+
+wasm=true
 root="."
-rm -rv *.o $root/out
+
+#### end config
+
+set -u # no references to undefined vars
+set -o pipefail
+IFS=$'\n\t'
+if $wasm; then
+  cc="em++"
+else
+  cc="g++"
+fi
+
+rm -rv $root/out ./*.{o,tmp,wasm,js,mjs,html,d.ts}
 
 srcdir="$root/src/"
 srcfiles=(
@@ -16,13 +32,18 @@ srcfiles=(
 # run.sh only had first 4 srcfiles
 
 # https://www.gnu.org/software/bash/manual/bash.html#Shell-Parameter-Expansion
-expanded_src_files="${srcfiles[@]/*/$srcdir&'.cpp'}"
-expanded_o_files="${srcfiles[@]/*/'./'&'.o'}"
+expanded_src_files=${srcfiles[*]/*/$srcdir&'.cpp'}
 
 echo compiling...
-g++ -O3 -I "$root/include/" -fopenmp -c $expanded_src_files ./test.cpp  && \
-g++ -O3 -I "$root/include/" -fopenmp $expanded_o_files ./test.o -o ./testc
-
-rm -v *.o
 mkdir $root/out/
-mv testc $root/out/
+
+if $wasm; then
+  set -x
+  # remove -g to minify
+  $cc -O3 -I $root/include/ -fopenmp $expanded_src_files ./test.cpp --emit-tsd types.d.ts -g -o ./testc.mjs
+  mv -v ./*.mjs ./*.wasm ./*.d.ts $root/out/
+else
+  set -x
+  $cc -O3 -I $root/include/ -fopenmp $expanded_src_files ./test.cpp -o ./testc
+  mv -v testc $root/out/
+fi
